fc8bf3584a23340ace79c0e85ad127cce9ca2209
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2024-10-03 16:44:56 -0300

    checkpoint

--- datos/pruebas.py
+++ datos/pruebas.py
@@ -4,3 +4,20 @@ b = str(a)
 print(b.split("', '"))
 f = open('out.csv', 'w', encoding='utf-8')
 f.write(str(a))
+
+a = 5000000000
+def poner_comas(n):
+    s = str(n)
+    rv = ''
+    coma = 0
+    for i in range(len(s)):
+        if coma == 2:
+            rv += s[-i-1]
+            rv += ','
+            coma = 0
+        else:
+            rv += s[-i-1]
+            coma += 1
+    return rv[::-1]
+b = poner_comas(a)    
+print(b)
