8e115cd1168c52698b2890926ba6f289cd80b74c
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2024-10-09 09:06:10 -0300

    beggining maqueta

--- datos/limpiar.py
+++ datos/limpiar.py
@@ -1,14 +1,18 @@
 import csv
 
-f = open("nuevos albums.csv", "r", encoding="utf-8")
-f_out = open("albums_2.csv", "w", encoding="utf-8") 
+f = open("albums_2.csv", "r", encoding="utf-8")
+f_out = open("albums.csv", "w", encoding="utf-8") 
 f_out.write("Año,Album,Streams,Generos,Valoracion\n")
 
+i = 0
+cont = 0
 for linea in f:
-    s = linea.replace("\"", "")
-    if s[-1] == ',':
-        s = s[:-1]
-    f_out.write(s)
+    if cont == 5:
+        cont = 0
+        i += 1
+    linea = str(2003 + i)+','+linea
+    f_out.write(linea)
+    cont += 1
 
 f.close()
 f_out.close()
